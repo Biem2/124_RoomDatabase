531e63f635f85ca73e6d0a6f1ad8bcb79b28cdeb
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 16:26:21 +0700

    Menambahkan juga membuat file ContainerApp

---
+++ app/src/main/java/com/example/a124_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,12 @@
+package com.example.a124_roomdatabase.repositori
+
+import android.content.Context
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context)
+    ContainerApp{
+        o
+    }
