2d298a90cef8083296243120654bc79aa5e168bd
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 16:57:17 +0700

    Menambahkan data class DetailSiswa dan fungsi konversi extension di EntryViewModel

--- app/src/main/java/com/example/a124_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a124_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.a124_roomdatabase.viewmodel
 
+import com.example.a124_roomdatabase.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon=telpon,
+)
