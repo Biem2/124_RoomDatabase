9fb89d6016743a0ffe9e44e6243f1d4dc000b90e
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 21:57:12 +0700

    Refactor EntryViewModel to include UI state management and validation logic

---
+++ app/src/main/java/com/example/a124_roomdatabase/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,18 @@
+
+package com.example.roomdatabase.view.route
+
+
+/**
+ * Interface dasar untuk menentukan route dan title halaman.
+ */
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable.
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman.
+     */
+    val titleRes: Int
+}
