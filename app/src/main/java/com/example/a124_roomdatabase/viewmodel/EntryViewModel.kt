e1fced447a4ea08915d0fe246b0ae6188cbbaabb
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 21:57:05 +0700

    Refactor EntryViewModel to include UI state management and validation logic

--- app/src/main/java/com/example/a124_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a124_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,29 +1,53 @@
 package com.example.a124_roomdatabase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.a124_roomdatabase.repositori.RepositoriSiswa
 import com.example.a124_roomdatabase.room.Siswa
 
-data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telpon: String = "",
-)
 
-fun DetailSiswa.toSiswa(): Siswa = Siswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon
-)
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
 
-fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
-fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon=telpon,
-)
+    private fun validasiInput(input: DetailSiswa): Boolean {
+        return with(input) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    suspend fun saveSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+
+    data class UIStateSiswa(
+        val detailSiswa: DetailSiswa = DetailSiswa(),
+        val isEntryValid: Boolean = false
+    )
+
+    data class DetailSiswa(
+        val id: Int = 0,
+        val nama: String = "",
+        val alamat: String = "",
+        val telpon: String = ""
+    )
+
+    fun DetailSiswa.toSiswa(): Siswa = Siswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+}
