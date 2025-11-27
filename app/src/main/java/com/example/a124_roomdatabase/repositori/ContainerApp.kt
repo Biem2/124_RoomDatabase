c2378b9abade45e172ee785205676213f946a06f
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 16:31:13 +0700

    Menambahkan juga memperbaiki file ContainerApp

--- app/src/main/java/com/example/a124_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a124_roomdatabase/repositori/ContainerApp.kt
@@ -1,12 +1,26 @@
 package com.example.a124_roomdatabase.repositori
-
+import android.app.Application
 import android.content.Context
+import androidx.compose.foundation.text.contextmenu.data.TextContextMenuData
+import com.example.a124_roomdatabase.room.DatabaseSiswa
 
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
-class ContainerDataApp(private val context: Context)
+class ContainerDataApp(private val context: Context):
     ContainerApp{
-        o
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
     }
+}
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context = this)
+    }
+}
