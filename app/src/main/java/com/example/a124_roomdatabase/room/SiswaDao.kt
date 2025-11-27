3b77dd538e0ebb3ef41bb69eb8785529be8612c8
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 16:21:48 +0700

    memnambahkan dan membuat SiswaDao

--- app/src/main/java/com/example/a124_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a124_roomdatabase/room/SiswaDao.kt
@@ -2,17 +2,15 @@ package com.example.a124_roomdatabase.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
 interface SiswaDao {
-    val OnConflictStragery: Any
-
-    @Query(value = "SELECT * from tbSiswa ORDER BY nama ASC")
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa() : Flow<List<Siswa>>
 
-    @Insert(onConflict = OnConflictStragery.IGNORE)
-    suspend fun Insert(Siswa : siswa)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
 }
