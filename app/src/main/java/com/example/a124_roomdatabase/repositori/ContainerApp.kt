11b656bbc261ff579a55816b7c0e9aa5ba118f27
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 21:58:02 +0700

    Menghapus import tidak terpakai dan memperbaiki format pada ContainerApp

--- app/src/main/java/com/example/a124_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a124_roomdatabase/repositori/ContainerApp.kt
@@ -1,26 +1,28 @@
 package com.example.a124_roomdatabase.repositori
+
 import android.app.Application
 import android.content.Context
-import androidx.compose.foundation.text.contextmenu.data.TextContextMenuData
 import com.example.a124_roomdatabase.room.DatabaseSiswa
 
-interface ContainerApp {
+interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
 }
 
 class ContainerDataApp(private val context: Context):
-    ContainerApp{
+    ContainerApp {
     override val repositoriSiswa: RepositoriSiswa by lazy {
         OfflineRepositoriSiswa(
-            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
-        )
+            DatabaseSiswa.getDatabase(context).siswaDao())
     }
 }
-class AplikasiSiswa : Application() {
+
+class AplikasiSiswa : Application(){
+
+
     lateinit var container: ContainerApp
 
     override fun onCreate() {
         super.onCreate()
-        container = ContainerDataApp(context = this)
+        container = ContainerDataApp(this)
     }
 }
