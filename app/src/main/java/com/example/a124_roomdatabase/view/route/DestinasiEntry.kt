8f8bf855a9027e45ece77614d379413ca1cc2482
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-27 21:58:25 +0700

    Menambahkan DestinasiEntry pada package route untuk navigasi entry siswa

---
+++ app/src/main/java/com/example/a124_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+
+package com.example.a124_roomdatabase.view.route
+import com.example.a124_roomdatabase.R
+import com.example.roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
